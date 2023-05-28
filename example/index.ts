2ffea620d8f30ec52d1435fbe252fafd7dcb6b55
Pi-Bouf <pierre.bouffier05@gmail.com>
2023-05-28 12:01:14 -0400

    Add performance sandbox

--- example/index.ts
+++ example/index.ts
@@ -1,9 +1,9 @@
-import { Action } from './enum/Action';
-import { Avatar } from './Avatar';
-import { AvatarPositionComputer } from './AvatarPositionComputer';
-import { Direction } from './enum/Direction';
-import { Engine } from '../src/Engine';
-import { Position3D } from '../src/geometry/Position3D';
+import {Action} from './enum/Action';
+import {Avatar} from './Avatar';
+import {AvatarPositionComputer} from './AvatarPositionComputer';
+import {Direction} from './enum/Direction';
+import {Engine} from '../src/Engine';
+import {Position3D} from '../src/geometry/Position3D';
 
 const sandbox = new Engine({
     width: 1000,
@@ -13,9 +13,9 @@ const sandbox = new Engine({
     mouseEventFrequency: 10000,
     autoResize: true,
     maxAnimationRate: 8,
-    maxDisplayRate: 60,
+    maxDisplayRate: 140,
     images: {
-        imageDomain: 'http://127.0.0.1:8081/images/',
+        imageDomain: 'http://127.0.0.1:8081/',
     },
 });
 
@@ -43,12 +43,16 @@ const action = [
 
 const avatar = ['avatar/archer_001.png', 'avatar/wizard_001.png', 'avatar/soldier_001.png'];
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 1000; i++) {
     const randomDirection = direction[Math.random() * 4 | 0];
     const randomAction = action[Math.random() * 8 | 0];
     const randomAvatar = avatar[Math.random() * 3 | 0];
 
     const player = new Avatar(randomAvatar, randomAction, randomDirection);
-    player.setPosition(new Position3D(Math.random() * 1700 | 0, Math.random() * 700 | 0));
+    player.setPosition(new Position3D(Math.random() * 1000 | 0, Math.random() * 700 | 0));
     sandbox.stage.addChild(player);
 }
+
+    // const player = new Avatar('avatar/wizard_001.png',Action.ATTACK, Direction.E);
+    // player.setPosition(new Position3D(100, 100));
+    // sandbox.stage.addChild(player);
